98885e68e50f3216fbdefc4cc5e9c14644efd3fb
Eliza_Aghasyan <yourgitemail@example.com>
2024-04-29 14:57:29 +0400

    SQL-Integration: Task 8

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import * as helmet from 'helmet';
 
 import { AppModule } from './app.module';
+import { Handler } from 'aws-lambda';
+import  ServerlessAdapter  from '@vendia/serverless-express';
+
 
 const port = process.env.PORT || 4000;
+let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,9 +17,19 @@ async function bootstrap() {
     origin: (req, callback) => callback(null, true),
   });
   app.use(helmet());
-
+  server = ServerlessAdapter(app.getHttpAdapter().getInstance());
+  
+  
   await app.listen(port);
 }
+
+export const lambdaHandler: Handler = async (event, context, callback) => {
+  if (!server) {
+    await bootstrap();
+  }
+  return server(event, context, callback);
+};
+
 bootstrap().then(() => {
   console.log('App is running on %s port', port);
 });
